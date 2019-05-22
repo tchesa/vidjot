dfcd6f1cb97a11f598e5d5501c277d6bc9156e62
cesar <cesar@usemobile.xyz>
2019-05-22 17:10:53 -0300

    register form validation

--- routes/users.js
+++ routes/users.js
@@ -11,4 +11,23 @@ router.get('/register', (req, res) => {
   res.render('users/register')
 })
 
+// register form POST
+router.post('/register', (req, res) => {
+  let errors = []
+  if (req.body.password !== req.body.confirmPassword) errors.push({text: 'Passwords do not match'})
+  if (req.body.password.length < 4) errors.push({text: 'Password must be at least 4 characters'})
+
+  if (errors.length > 0) {
+    res.render('users/register', {
+      errors: errors,
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+      confirmPassword: req.body.confirmPassword
+    })
+  } else {
+    res.send('passed')
+  }
+})
+
 module.exports = router
