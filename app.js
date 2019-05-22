fe163fd4e9d14b835b1fcb2009b82e919f6de5aa
cesar <cesar@usemobile.xyz>
2019-05-22 14:03:50 -0300

    server side form validation

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 
 const app = express()
 
@@ -21,6 +22,10 @@ const Idea = mongoose.model('ideas')
 app.engine('handlebars', exphbs({defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
 
+// body parser middleware
+app.use(bodyParser.urlencoded({ extend: false }))
+app.use(bodyParser.json()) // parse application/json
+
 // index route
 app.get('/', (req, res) => {
   const title = 'Welcome'
@@ -38,6 +43,23 @@ app.get('/about', (req, res) => {
 app.get('/ideas/add', (req, res) => {
 
   res.render('ideas/add')
+})
+
+// process form - handle post idea
+app.post('/ideas', (req, res) => {
+  let errors = []
+  if (!req.body.title) errors.push({text: 'Please add a title'})
+  if (!req.body.details) errors.push({text: 'Please add some details'})
+
+  if (errors.length > 0) {
+    res.render('ideas/add', {
+      errors: errors,
+      title: req.body.title,
+      details: req.body.details
+    })
+  } else {
+    res.send('passed')
+  }
 })
 
 const port = 5000
