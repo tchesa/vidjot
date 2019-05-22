5f77b3e741794b07dbde0d0406b2440070c1bd61
cesar <cesar@usemobile.xyz>
2019-05-22 15:05:19 -0300

    remove idea data

--- app.js
+++ app.js
@@ -100,6 +100,13 @@ app.put('/ideas/:id', (req, res) => {
   })
 })
 
+// delete form process
+app.delete('/ideas/:id', (req, res) => {
+  Idea.deleteOne({_id: req.params.id}).then(() => {
+    res.redirect('/ideas')
+  })
+})
+
 const port = 5000
 
 app.listen(port, () => {
