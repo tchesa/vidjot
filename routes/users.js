b17be19dabbdd82c4d64e3b3cb0396e524a35443
cesar <cesar@usemobile.xyz>
2019-05-22 18:44:56 -0300

    logout function

--- routes/users.js
+++ routes/users.js
@@ -70,4 +70,11 @@ router.post('/register', (req, res) => {
   }
 })
 
+// logout user
+router.get('/logout', (req, res) => {
+  req.logout()
+  req.flash('success_msg', 'You are logged out')
+  res.redirect('/users/login')
+})
+
 module.exports = router
