bea26128f4c562c06678562be490e15ab8d797e7
cesar <cesar@usemobile.xyz>
2019-05-22 11:27:25 -0300

    working with templates

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
+
 const app = express()
+
+// handlebars middleware
+app.engine('handlebars', exphbs({defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
 
 // Index Route
 app.get('/', (req, res) => {
-  res.send('INDEX')
+  const title = 'Welcome'
+  res.render('index', {
+    title: title
+  })
 })
 
 // About Route
 app.get('/about', (req, res) => {
-  res.send('ABOUT')
+  res.render('about')
 })
 
 const port = 5000
