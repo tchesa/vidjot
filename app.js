d727b9b0d64f05616ce2d149de5e455b57692499
cesar <cesar@usemobile.xyz>
2019-05-22 12:42:53 -0300

    install mongoose & connect

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const mongoose = require('mongoose')
 
 const app = express()
+
+// Map global promise - get rid of warning
+mongoose.Promise = global.Promise
+// connect to mongoose
+mongoose.connect('mongodb://localhost:5001/vidjot-dev', {
+  useNewUrlParser: true
+}).then(() => {
+  console.log('MongoDB Connected...')
+}).catch(err => console.log(err))
 
 // handlebars middleware
 app.engine('handlebars', exphbs({defaultLayout: 'main'}))
